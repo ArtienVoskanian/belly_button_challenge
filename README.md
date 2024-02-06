70ef0ccd148655796b0a8135903c8374106953ca
ArtienVoskanian <arvoskani@gmail.com>
2024-02-05 19:18:28 -0800

    Completed final section and README file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # belly_button_challenge
+
+The primary responsibilities of effective data analysts are persuasive truth telling and storytelling. A powerful tool to aid in these responsibilities is interactive data visualizations. In this challenge, a data set containing various information about microbial species (and the people’s bellybutton they were found in) was given. 
+
+We were tasked with creating interactive dashboards, bar charts, and bubble graphs that would update with relevant information and visuals when a new sample in the data is selected. These tasks were completed using JavaScript, JavaScript libraries such as Plotly, and html code.
+
+The forEach() function that is used in the code was discovered on the following website:
+
+https://www.w3schools.com/jsref/jsref_foreach.asp
--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,5 @@
 // Create a variable to store the url 
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
-
 // Initial funciton to display default data
 function init(){
     
@@ -14,29 +13,40 @@ function init(){
         let names = data.names;
 
         // Assigning the samples to the dropdown menu
-        names.forEach(function(id){
+        names.forEach((id) =>{
             dropMenu.append("option").text(id).property("value",id);
         });
 
         // New variable assigned to the first name
         let name = names[0];
 
+        
+
         //Now call the functions that will be defined later to create our demographics, bar chart, and bubble chart
-        metadata(name)
+        
         demographics(name);
         barChart(name);
         bubble(name);
+
+              
        
 
     
     });
-
+    // Add event listener for dropdown change
+    dropMenu.on("change", function() {
+        // Get the selected value
+        let newSample = dropMenu.property("value");
+        
+        // Call the function to update all plots
+        newInput(newSample);
+    });
 };
 
 // Now we define our demographics function which will populate the demographics panel
-function metadata(userInput){
+function demographics(userInput){
 
-    d3.json(url).then(function(data){
+    d3.json(url).then((data)=>{
 
         //Isolate the metadata information into a new variable
         let metaData = data.metadata;
@@ -45,10 +55,13 @@ function metadata(userInput){
         let filtered = metaData.filter((meta) =>meta.id == userInput);
         let sampleValue = filtered[0];
 
+        //Clear out the metadata
+        d3.select("#sample-metadata").html("");
+
         // Using Object.entries which is a built in method, we can assing key/value pairings to the panel
         Object.entries(sampleValue).forEach(([key,value])=>{
 
-        d3.select("#sample-metadata").append("h5").text(`${key}:${value}`);
+        d3.select("#sample-metadata").append("h5").text(`${key}: ${value}`);
         
         });
 
@@ -94,10 +107,61 @@ function barChart(userInput){
         };
 
         Plotly.newPlot("bar",[trace1],layout)
+    });
 
+};
 
+// Build the bubble chart function
+function bubble(userInput){
+
+    d3.json(url).then(function(data){
+
+        // Store the sample data in a seperate variable
+        let sample = data.samples;
+
+        //Filter the sample data based on the inputed id. Select the first index from the array
+        let sampleSelected = sample.filter(value => value.id == userInput);
+        let isolatedData = sampleSelected[0];
+
+        // With the wanted information now isolated, store sample_values, otu_ids, and otu_labels into variables 
+        let sampleValues = isolatedData.sample_values;
+        let otuIds = isolatedData.otu_ids;
+        let otuLabels = isolatedData.otu_labels;
+
+        //Setup the trace for the bubble chart, the layout, and call on plotly to display it
+        let trace2 = {
+            x: otuIds,
+            y: sampleValues,
+            text: otuLabels,
+            mode: "markers",
+            marker: {
+                size: sampleValues,
+                color: otuIds,
+                colorscale: "Viridis" 
+            }
+        };
+
+        let layout = {
+            hovermode: "closest",
+            xaxis: {title: "OTU ID"},
+        };
+
+        Plotly.newPlot("bubble",[trace2],layout)
 
     });
 
+};
+
+//Create a function that updates all plots and boards when a new sample is selected
+function newInput(newSample){
+
+    
+
+    demographics(newSample);
+    barChart(newSample);
+    bubble(newSample);
+
 
 };
+
+init();
